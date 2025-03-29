2603f03b40768d8da24c2b3c88d31543ebe12f5c
nevil <nevil.oporo@student.moringaschool.com>
2025-03-29 22:44:36 +0300

    fetch

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+   return fetch('https://anapioficeandfire.com/api/books')
+          .then(response => response.json())
+          .then(data => {
+              renderBooks(data);
+              return data; 
+          })
+          .catch(console.error);
   
 }
 
@@ -7,10 +14,12 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
+
+fetchBooks();
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
